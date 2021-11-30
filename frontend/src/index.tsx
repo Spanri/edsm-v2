953d039de1dbdd42ef7f13d9e32a172c36be205d
spanri <kozlova9v@mail.ru>
2021-11-30 16:21:55 +0300

    docker settings

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { STATE_NAME } from "@/helpers/lsState.helper"
 import "./index.scss"
+import * as serviceWorker from './serviceWorker';
 
 import App from "./App"
 
@@ -30,3 +31,8 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById("root")
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
